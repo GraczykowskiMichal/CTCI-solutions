9c580f0916dcb23dbadb104173fa207266435beb
Michal Graczykowski <graczykowskimichal@gmail.com>
2018-03-15 14:15:44 +0100

    kth to the last

---
+++ Chapter 2 - Linked Lists/2.2_kthToTheLast/Solution.java
@@ -0,0 +1,28 @@
+package com.michalgraczykowski;
+
+public class Solution {
+
+    Node kthToTheLast(Node head, int k) {
+        if (k < 1) throw new IllegalArgumentException("k should be positive");
+        int listLength = calculateLength(head);
+        if (listLength < k) throw new IllegalArgumentException("k should be greater or equal to length of the list");
+        return getElemAtPosition(head, listLength - k);
+    }
+
+    private int calculateLength(Node head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+
+    private Node getElemAtPosition(Node head, int position) {
+        while (position > 0) {
+            head = head.next;
+            position--;
+        }
+        return head;
+    }
+}
